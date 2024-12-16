516e129acc95273219ebf0d27dba6a336daf7197
NatchaLatte <foxteary@gmail.com>
2024-12-16 23:55:36 +0700

    LIST USERS

--- tests/reqres.spec.ts
+++ tests/reqres.spec.ts
@@ -1,8 +1,22 @@
 import { test, expect } from '@playwright/test'
+import Ajv2020  from 'ajv/dist/2020'
+import { listUsers, singleUser } from '../schema/get-api.schema'
+
+const ajv = new Ajv2020 ()
 
 test.describe('GET API', () => {
   test('LIST USERS', async ({ request }) => {
-    const response = await request.get('/api/users/2')
+    const response = await request.get('/api/users?page=2')
+    
+    const stauts = response.status()
+    expect(stauts).toBe(200)
+
+    const headers = response.headers()
+    expect(headers['content-type']).toContain('application/json')
+
+    const body = await response.json()
+    const validate =ajv.compile(listUsers)
+    expect(validate(body)).toBe(true)
   })
 })
 
