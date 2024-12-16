8e83e9bc1b6d869a4b89bf274cff7ccec5ff14de
NatchaLatte <foxteary@gmail.com>
2024-12-17 00:06:25 +0700

    SINGLE <RESOURCE>

--- schema/get-api.schema.ts
+++ schema/get-api.schema.ts
@@ -92,3 +92,28 @@ export const listResource = {
     },
     required: ['page', 'per_page', 'total', 'total_pages', 'data', 'support']
 }
+
+export const singleResource = {
+    type: 'object',
+    properties: {
+        data: {
+            type: 'object',
+            properties: {
+                id: { type: 'integer' },
+                name: { type: 'string' },
+                year: { type: 'integer' },
+                color: { type: 'string' },
+                pantone_value: { type: 'string' }
+            },
+            required: ['id', 'name', 'year', 'color', 'pantone_value']
+        },
+        support: {
+            type: 'object',
+            properties: {
+                url: { type: 'string' },
+                text: { type: 'string' }
+            },
+            required: ['url', 'text']
+        }
+    }
+}
--- tests/reqres.spec.ts
+++ tests/reqres.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 import Ajv2020  from 'ajv/dist/2020'
-import { listResource, listUsers, singleUser } from '../schema/get-api.schema'
+import { listResource, listUsers, singleResource, singleUser } from '../schema/get-api.schema'
 
 const ajv = new Ajv2020 ()
 
@@ -54,6 +54,19 @@ test.describe('GET API', () => {
 
     const body = await response.json()
     const validate = ajv.compile(listResource)
+    expect(validate(body)).toBe(true)
+  })
+  test('SINGLE <RESOURCE>', async ({ request }) => {
+    const response = await request.get('/api/unknown/2')
+    
+    const stauts = response.status()
+    expect(stauts).toBe(200)
+
+    const headers = response.headers()
+    expect(headers['content-type']).toContain('application/json')
+
+    const body = await response.json()
+    const validate = ajv.compile(singleResource)
     expect(validate(body)).toBe(true)
   })
 })
